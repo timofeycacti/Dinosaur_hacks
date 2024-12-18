772f7f8a7542ae88bf9444aaab0f33af9e46ad12
timofeycacti <149524909+timofeycacti@users.noreply.github.com>
2024-12-18 20:28:50 +0100

    Dinohacks_v2
    Добавляет замедление, при приближении к кактусу ломает кактусы на поле и даёт несколько очков

--- dinohacks.py
+++ dinohacks.py
@@ -1,63 +1,29 @@
 import subprocess
 import time
 import pychrome
-from pynput import keyboard  # Подключаем pynput для отслеживания нажатий клавиш
+from pynput import keyboard
 import threading
 import random
 
-# Запуск браузера
-chrome_path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"  # macOS
-subprocess.Popen([
-    chrome_path,
-    "--remote-debugging-port=9222",  # Порт для подключения
-    "--user-data-dir=/tmp/ChromeDev",  # Временная директория профиля
-    "--no-first-run",  # Пропускаем первый запуск
-    "--disable-popup-blocking",  # Отключаем блокировку всплывающих окон
-    "--enable-fullscreen"
-])
-time.sleep(2)  # Ожидание запуска браузера
-
-# Подключение к браузеру
-browser = pychrome.Browser(url="http://127.0.0.1:9222")
+browser = pychrome.Browser(url="http://127.0.0.1:1234")
 print(__file__)
-# Открытие новой вкладки с определённым URL
-url = "chrome://dino"  # Укажите нужный адрес
-tab = browser.new_tab(url)
+tab = browser.new_tab("chrome://dino")
 tab.start()
-tab.Page.enable()
 
-# Изменение цвета фона
-
-def XDfunction(speed):
-    global tab
-    tab.Runtime.evaluate(expression=f'Runner.instance_.tRex.jumping = {speed}')
-
-# Глобальная переменная для отслеживания состояния Shift
-shift_pressed = False
-
-# Обработчики событий нажатия и отпускания клавиш
-def on_press(key):
-    global shift_pressed
-    if key == keyboard.Key.shift:
-        shift_pressed = True
-
-def on_release(key):
-    global shift_pressed
-    if key == keyboard.Key.shift:
-        shift_pressed = False
-
-# Функция для выполнения действий в цикле
 def main_loop():
+    global tab
+    cactiX=tab.Runtime.evaluate(expression="Runner.instance_.setSpeed(3)")
     while True:
-        if shift_pressed:
-            tab.Runtime.evaluate(expression="Runner.instance_.setSpeed(0.01)")
-            while shift_pressed:
-                tab.Runtime.evaluate(expression="Runner.instance_.distanceRan +=1000000")
-        time.sleep(0.1)  # Задержка для снижения нагрузки на процессор
-        tab.Runtime.evaluate(expression="Runner.instance_.setSpeed(6)")
-# Запуск слушателя в отдельном потоке
-listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-listener.start()
+        try:
+            cactiX=tab.Runtime.evaluate(expression="Runner.instance_.horizon.obstacles[0].xPos")["result"]["value"]
+            print(cactiX)
+            if cactiX <= 200:
+                tab.Runtime.evaluate(expression="Runner.instance_.horizon.obstacles.pop(0)")
+                tab.Runtime.evaluate(expression="Runner.instance_.distanceRan+=1000")
+        except:
+            print("didn't find it")
 
-# Запуск основного цикла
+        time.sleep(0.05)
+
+
 main_loop()
